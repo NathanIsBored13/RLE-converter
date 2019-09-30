0246635788ae5c3c1d5bb77ea96b508bc8ece1dc
nathan smith <nathan.smith@canterburians.org.uk>
2019-09-30 23:25:23 +0100

    Finished ToRLE converter and file IO

--- RLE converter/Program.cs
+++ RLE converter/Program.cs
@@ -8,30 +8,46 @@ namespace RLE_converter
     {
         static void Main(string[] args)
         {
-            string file = "";
-            if (File.Exists(Environment.CurrentDirectory + "\\ascii.txt")) file = "\\ascii.txt";
-            else if (File.Exists(Environment.CurrentDirectory + "\\RLE.txt")) file = "\\RLE.txt";
-            FileStream fs = File.Open(Environment.CurrentDirectory + file, FileMode.Open, FileAccess.Read, FileShare.None);
+            if (!File.Exists(Environment.CurrentDirectory + "\\input.txt"))
+            {
+                Console.WriteLine("input file does not exist, exiting");
+                Environment.Exit(0);
+            }
+            FileStream fs = File.Open(Environment.CurrentDirectory + "\\input.txt", FileMode.Open, FileAccess.Read, FileShare.None);
             byte[] buffer = new byte[fs.Length];
             fs.Read(buffer, 0, (int)fs.Length);
             fs.Close();
             string[] read = Encoding.Default.GetString(buffer).Split('\n');
-            foreach (string line in read) File.AppendAllText(Environment.CurrentDirectory + file, file == "\\ascii.txt"? ToRLE(line): ToASCII(line));
+            string input;
+            File.Create(Environment.CurrentDirectory + "\\output.txt").Close();
+            do
+            {
+                Console.Write("[1]converte from ASCII, [2]converte from RLE: ");
+                input = Console.ReadLine();
+                if (input == "1") foreach (string line in read) File.AppendAllText(Environment.CurrentDirectory + "\\output.txt", ToRLE(line));
+                else if (input == "2")
+                {
+
+                }
+                else Console.WriteLine("\nonly enter 1 or 2\n");
+            } while (!"12".Contains(input));
         }
         static string ToRLE(string line)
         {
             int run = 0;
             string ret = "";
-            for (int i = 0; i < line.Length-1; i++)
+            char current = line[0];
+            for (int i = 0; i < line.Length; i++)
             {
-                char current = line[i];
-                if (line[i] == line[i + 1]) run++;
-                else
+                if (line[i] == current) run++;
+                else //if (line[i] != current)
                 {
-                    //ret += String.Format("{0}{1}", line[i], );
+                    ret += String.Format("{0}{1:00}", current, run);
+                    run = 1;
+                    current = line[i];
                 }
             }
-            return ret;
+            return ret += String.Format("{0}{1:00}", current, run);
         }
         static string ToASCII(string line)
         {
