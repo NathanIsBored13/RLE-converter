1ec7b7a9ece9f07c36577d367c8d88d307a3c91d
Nathan Smith <K036659@eltham-college.org.uk>
2019-10-03 12:54:42 +0100

    Reworked menue and added RLE entery

--- RLE converter/Program.cs
+++ RLE converter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace RLE_converter
 {
@@ -8,39 +9,90 @@ namespace RLE_converter
     {
         static void Main(string[] args)
         {
-            // reads the file
-            string[] file = ReadFile(Environment.CurrentDirectory + "\\input.txt");
-            // if the file was readable, run the main program with the file as an input
-            if (file != null) Run(file);
+            Run();
         }
-        static void Run(string[] file)
+        static void Run()
         {
             string input;
+            bool exit = false;
             // make the output file, removing it if it currently exists
             File.Create(Environment.CurrentDirectory + "\\output.txt").Close();
             do
             {
-                Console.Write("[1]converte from ASCII, [2]converte from RLE: ");
+                Console.Write("[1]Enter RLE, [2]Display ASCII art file, [3]Convert to ASCII art [4] Convert to RLE [5]exit: ");
                 input = Console.ReadLine();
                 // if the user entered a valid input
-                if ("12".Contains(input))
+                if (input == "1")
                 {
+                    do
+                    {
+                        Console.Write("how many lines of RLE do you wish to enter: ");
+                        input = Console.ReadLine();
+                        if (!input.All(Char.IsDigit))
+                        {
+                            Console.WriteLine("only enter digits");
+                            input = "0";
+                        }
+                        else if (Convert.ToInt32(input) < 2) Console.WriteLine("the number of lines must be greater then 2");
+                    } while (Convert.ToInt32(input) < 2);
+                    string[] ASCII = new string[Convert.ToInt32(input)];
+                    for (int i = 0; i < Convert.ToInt32(input); i++) ASCII[i] = Converter(true, EnterRLE());
+                    foreach (string line in ASCII) Console.WriteLine(line);
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("enter the ASCII file name to be displayed");
+                    string[] file = ReadFile(string.Format("{0}\\{1}.txt", Environment.CurrentDirectory, Console.ReadLine()));
+                    foreach (string line in file) Console.WriteLine(file);
+                }
+                else if ("34".Contains(input))
+                {
+                    Console.Write("enter the {0} file name: ", input == "2" ? "ASCII" : "RLE");
+                    string[] file = ReadFile(string.Format("{0}\\{1}.txt",Environment.CurrentDirectory, Console.ReadLine()));
                     for (int i = 0; i < file.Length; i++)
                     {
                         // removes any newline and carrage return charecters then passes it to the Converter function alongside the input
-                        string print = Converter(input, System.Text.RegularExpressions.Regex.Replace(file[i], "\r|\n", ""));
+                        string print = Converter(input == "3", System.Text.RegularExpressions.Regex.Replace(file[i], "\r|\n", ""));
                         // writes it to the output file, adding newline charecters if it is not the last line
-                        File.AppendAllText(Environment.CurrentDirectory + "\\output.txt", string.Format("{0}{1}", print, i < file.Length - 1? "" : "\n"));
+                        File.AppendAllText(Environment.CurrentDirectory + "\\output.txt", string.Format("{0}{1}", print, i < file.Length - 1 ? "" : "\n"));
                         // prints what was written to the file
                         Console.WriteLine(print);
                     }
                 }
+                else if (input == "5") Console.WriteLine("\nExiting...\n");
                 // if the input was not valid, say so
-                else Console.WriteLine("\nonly enter 1 or 2\n");
-            } while (!"12".Contains(input));
+                else
+                {
+                    Console.WriteLine("\nplease only enter 1, 2, 3, 4 or 5\n");
+                    exit = true;
+                }
+            } while (!exit);
+        }
+        static string EnterRLE()
+        {
+            bool valid = true;
+            string input = "";
+            do
+            {
+                Console.Write("enter a line of RLE: ");
+                input = Console.ReadLine();
+                if (input.Length % 3 != 0) valid = false;
+                else
+                {
+                    for (int i = 0; i < input.Length; i += 3)
+                    {
+                        if (!input.Substring(i, 2).All(char.IsDigit))
+                        {
+                            Console.WriteLine("this line of RLE is invalid");
+                            valid = false;
+                        }
+                    }
+                }
+            } while (!valid);
+            return input;
         }
         // passes the line to the appropriate function then passes the output back up
-        static string Converter(string option, string line) => option == "1"? ToRLE(line) : ToASCII(line);
+        static string Converter(bool option, string line) => option? ToASCII(line) : ToRLE(line);
         static string ToRLE(string line)
         {
             int run = 1;
